328e9acd9480d75608d82445347bf4926b0e3e82
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-10-29 20:36:39 +0100

    Exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let tempCelsius = Math.round((((x - 32) * 5) / 9) * 10) / 10;
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (y) {
+  let tempFahr = Math.round(((y * 9) / 5 + 32) * 10) / 10;
+  return tempFahr;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
