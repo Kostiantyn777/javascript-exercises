9c9e3aed6479a45525cfd2e413ae2a1af5709b5b
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-10-29 11:44:02 +0100

    Exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (defaultArg1 = [], ...optionalArg2) {
+  
+    let newArray = defaultArg1.filter((elem) => !optionalArg2.includes(elem));
+  return newArray;
 };
 
 // Do not edit below this line
