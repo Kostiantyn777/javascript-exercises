b8e18912446d3ecdd7d370ab8bca7b0c97472f51
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-09 16:26:31 +0100

    Exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringToLowerCase = string.toLowerCase();
 
+  //Remove punctuation and word breaks
+  let punctuation = /[\.,?!\s]/g;
+  let stringNoPunctuation = stringToLowerCase.replace(punctuation, "");
+  console.log(stringNoPunctuation);
+
+  let reverseString = stringNoPunctuation.split("").reverse().join("");
+
+  if (stringNoPunctuation === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
