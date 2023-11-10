62dbe4ca1d96dde58fc6b1ab7b1893367fe0f75c
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-10 11:16:17 +0100

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let sortedArray = arr.sort((person1, person2) => {
+    const currentYear = new Date().getFullYear();
 
+    const age1 = person1.yearOfDeath
+      ? person1.yearOfDeath - person1.yearOfBirth
+      : currentYear - person1.yearOfBirth;
+
+    const age2 = person2.yearOfDeath
+      ? person2.yearOfDeath - person2.yearOfBirth
+      : currentYear - person2.yearOfBirth;
+
+    return age2 - age1;
+  });
+  return sortedArray[0];
 };
-
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
