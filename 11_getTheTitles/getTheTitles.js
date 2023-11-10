4ad43bba4697cdf55b922fc801d3124997fb4aa4
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-10 09:54:54 +0100

    Exercise 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (arr) {
+  return arr.map((el) => el.title);
 };
 
 // Do not edit below this line
