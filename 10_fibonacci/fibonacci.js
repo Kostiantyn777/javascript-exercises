6908c899850d8d476964b7da0ec787d878e497b1
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-10 09:40:51 +0100

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  let fibonacciArray = [];
+  fibonacciArray[0] = 0;
+  fibonacciArray[1] = 1;
 
+  index = parseInt(index);
+
+  if (index < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 2; i <= index; i++) {
+    fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
+  }
+  return fibonacciArray[index];
 };
 
 // Do not edit below this line
